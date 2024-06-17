b42a23a8bbf65fa7a4f23e203b8a8656d3ce37d1
pramitsingh0 <pramitsingh0@gmail.com>
2024-06-17 15:55:30 +0530

    [F] Initial Commit

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,28 @@
+import Layout from "@/components/Layout/Layout";
+import Home from "@/pages/Home/Home";
+import Product from "@/pages/Product/Product";
+import Products from "@/pages/Products/Products";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+    ],
+  },
+]);
+
+export default router;
